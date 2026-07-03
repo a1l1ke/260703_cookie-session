1569d1da811bb74aa9ec4c371fc95259c886320f
a1l1ke <onetenthplease@gmail.com>
2026-07-03 10:51:38 +0900

    docs: 쿠키 학습 정리 문서 추가 및 서블릿 보안 설정 보완

--- src/main/java/com/example/cookiesession/step1/CookieServlet.java
+++ src/main/java/com/example/cookiesession/step1/CookieServlet.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class CookieServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Cookie[] cookies = req.getCookies();
+        Cookie[] cookies = req.getCookies(); // Spring 가보면 전용 유틸리티
         readCookies(cookies);
         // 쿠키를 보내려면
         resp.addCookie(new Cookie("firstCookie", "Hello_Cookie"));
@@ -34,8 +34,22 @@ public class CookieServlet extends HttpServlet {
         // 세션 쿠키 -> 현재 브라우저가 켜 있는 동안
 //        cookie.setMaxAge(16); // 16초 동안 유지
         // 0보다 큰 양수는 초. 60 * 60 * 24 하루. 86400
-        cookie.setMaxAge(60 * 60 * 24); // 하루 유지
+//        cookie.setMaxAge(60 * 60 * 24); // 하루 유지
+//        cookie.setMaxAge(0); // 쿠키를 삭제하고 싶을 때
 //        cookie.setMaxAge(86400);
+        // 3. 보안
+//        cookie.setHttpOnly(false);
+        cookie.setHttpOnly(true);
+        // document.cookie
+//        cookie.setSecure(false); // http도 허용
+        cookie.setSecure(true); // https만 허용 + localhost까진 허용
+        // 3-2. Samesite <- CSRF => 피싱사이트 방지
+//        cookie.setAttribute("SameSite", "None"); // Secure
+        // 다른 출처의 요청에도 전송한다 (다른 사이트에서 Form, Fetch 등으로 전송)
+//        cookie.setAttribute("SameSite", "Strict"); // 동일출처만 쿠키 전송 (Same Origin -> 프로토콜, 도메인, 포트까지 같아야 같은 오리진)
+//        cookie.setAttribute("SameSite", "Lax"); // 몇몇 조건에만 조건부 허용
+        // GET 요청. 유저가 링크/리다이렉트로 이동 -> 같은 출처가 아니더라도 허용.
+        // https://developer.mozilla.org/ko/docs/Web/HTTP/Reference/Headers/Set-Cookie#samesitesamesite-value
         return cookie;
     }
 
