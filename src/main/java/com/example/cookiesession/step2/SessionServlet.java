6bd42449c7fcf66691270143133cf6a3c83334ad
a1l1ke <onetenthplease@gmail.com>
2026-07-03 11:03:58 +0900

    feat: 세션 기능 구현을 위한 SessionServlet 및 JSP 파일 추가

---
+++ src/main/java/com/example/cookiesession/step2/SessionServlet.java
@@ -0,0 +1,17 @@
+package com.example.cookiesession.step2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/session")
+public class SessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/step02/session.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/step02/session.jsp
@@ -0,0 +1,13 @@
+<%-- webapp/WEB-INF/views/step01/cookie.jsp --%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Session</title>
+</head>
+<body>
+<h1>Session</h1>
+<p>
+
+</p>
+</body>
+</html>
